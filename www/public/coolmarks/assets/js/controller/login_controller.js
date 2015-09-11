bfcc5b4601ca4c33bcc4bc43eff1a2ee8bfe7e05
xiachen <chengpohi@gmail.com>
2015-09-11 22:52:52 +0800

    DONE:redirect login successfully. --- TODO:ipad

--- www/public/coolmarks/assets/js/controller/login_controller.js
+++ www/public/coolmarks/assets/js/controller/login_controller.js
@@ -1,12 +1,12 @@
 var loginForm = angular.module('login', []);
 
-loginForm.controller('loginCtrl', function($scope, loginService, $http) {
+loginForm.controller('loginCtrl', function($scope, loginService, $http, $window) {
     var user = {name: "", email: "", password: ""};
     $scope.user = user;
 
     $scope.userLogin = function() {
         loginService.userLogin($scope.user).success(function(data, status, headers, config) {
-            alert(data);
+            $window.location.href = "/index.html";
 		}).error(function(data, status, headers, config) {
 		    alert(data);
 		});
