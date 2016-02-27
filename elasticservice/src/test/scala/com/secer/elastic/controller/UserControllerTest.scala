d702ba74f54152e6ca7d9d732ebbff6b2a37092f
Xiang Chen <chengpohi@gmail.com>
2016-02-27 10:52:22 +0800

    DONE:fix user test---TODO:

--- elasticservice/src/test/scala/com/secer/elastic/controller/UserControllerTest.scala
+++ elasticservice/src/test/scala/com/secer/elastic/controller/UserControllerTest.scala
@@ -3,26 +3,27 @@ package com.secer.elastic.controller
 import com.github.chengpohi.controller.{UserController, ElasticController}
 import com.github.chengpohi.model.User
 import com.github.chengpohi.util.HashUtil
-import org.scalatest.FlatSpec
+import org.scalatest.{BeforeAndAfter, FlatSpec}
 
 /**
  * BookMark model
  * Created by com.github.chengpohi on 7/29/15.
  */
-class UserControllerTest extends FlatSpec {
+class UserControllerTest extends FlatSpec with BeforeAndAfter{
   val user = User("com.github.chengpohi", Some("com.github.chengpohi@gmail.com"), Some("123QQQ"))
   val user1 = User("com.github.chengpohi", Some("com.github.chengpohi@gmail.com"), Some("123456"))
 
-  "UserController " should " create user info" in {
-    val resultId = UserController.createUserInfo(user)
-    assert(resultId != null)
+  before {
+    UserController.createUserInfo(user)
   }
-
 
   "UserController " should " validate user info with wrong password" in {
     val userWithoutCorrectPassword = User("", Some("com.github.chengpohi@gmail.com"), Some("123456"))
     Thread.sleep(1000)
     assert(UserController.validateUserLogin(userWithoutCorrectPassword) == null)
   }
-  ElasticController.deleteIndexByIndexName(user.name)
+
+  after {
+    ElasticController.deleteIndexByIndexName(user.name)
+  }
 }
