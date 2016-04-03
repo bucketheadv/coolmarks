fc5fd530d178aca136f79d51b96677c3cf37f4c6
Xiang Chen <chengpohi@gmail.com>
2016-04-04 00:50:30 +0800

    DONE:local data---TODO:

--- elasticservice/src/main/scala/com.github.chengpohi/ElasticClientConnector.scala
+++ elasticservice/src/main/scala/com.github.chengpohi/ElasticClientConnector.scala
@@ -7,14 +7,19 @@ import com.typesafe.config.ConfigFactory
 import org.elasticsearch.common.settings.ImmutableSettings
 
 /**
- * Created by com.github.chengpohi on 3/19/15.
- */
+  * Created by com.github.chengpohi on 3/19/15.
+  */
 object ElasticClientConnector {
   lazy val indexConfig = ConfigFactory.load("application.conf").getConfig("elastic")
-  lazy val settings = ImmutableSettings.settingsBuilder().put("cluster.name", indexConfig.getString("cluster.name")).build()
+  lazy val settings = ImmutableSettings.settingsBuilder()
+    .put("http.enabled", "false")
+    .put("transport.enable", "false")
+    .put("path.home", indexConfig.getString("path.home"))
+    .put("cluster.name", indexConfig.getString("cluster.name"))
+    .build()
 
   val host: String = indexConfig.getString("host")
   val port: Int = indexConfig.getInt("port")
   val uri = ElasticsearchClientUri(s"elasticsearch://$host:$port")
-  val client = ElasticClient.remote(settings, uri)
+  val client = ElasticClient.local(settings)
 }
